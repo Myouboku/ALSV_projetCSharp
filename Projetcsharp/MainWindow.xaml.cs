7c88e905d707669a16587c0b4c697082f8252f32
Hugo Avril <hugo.avril@outlook.fr>
2022-03-08 17:41:32 +0100

    recup login + mdp & fonction sha256 mdp

--- Projetcsharp/MainWindow.xaml.cs
+++ Projetcsharp/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.Windows;
 using System.Data.SqlClient;
 using System;
+using System.Diagnostics;
+using XSystem.Security.Cryptography;
+using System.Text;
+using System.Data;
 
 namespace Projetcsharp
 {
@@ -14,23 +18,48 @@ namespace Projetcsharp
             InitializeComponent();
         }
 
+        // hash les MDP en sha256
+        public static string getHashSha256(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+
         private void btnConnexion_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 string reqConnect = @"Server=localhost;Database=ALSV_BDDCSHARP;User=sa;Password=info;";
                 SqlConnection connexion;
-
                 connexion = new SqlConnection(reqConnect);
                 connexion.Open();
-                MessageBox.Show("Connexion réussie");
+
+                string nomUtilisateur = lblNomUser.Text;
+                string mdp = getHashSha256(lblMDP.Text);
+
+                if (true)
+                {
+                    MedicWindow window = new();
+                    window.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Identifiant ou mot de passe incorrecte");
+                }
+
+                // MessageBox.Show("Connexion réussie");
                 connexion.Close();
-                MedicWindow window = new();
-                window.Show();
             }
-            catch (Exception erreur)
+            catch (Exception erreur2)
             {
-                MessageBox.Show("Connexion échouée :\n\n" + erreur);
+                MessageBox.Show("Connexion échouée :\n\n" + erreur2);
             }
         }
     }
