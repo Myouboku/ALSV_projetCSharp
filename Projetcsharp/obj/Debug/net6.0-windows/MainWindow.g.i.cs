ffe22c6834a8896928ca2e6589324975c4aa65cb
TrafiKen <kenzo.salaun@campus-la-chataigneraie.org>
2022-03-22 10:35:07 +0100

    Tentative résolution conflit #1

--- Projetcsharp/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Projetcsharp/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace Projetcsharp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Projetcsharp;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Projetcsharp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
