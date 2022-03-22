00001af4b717c31a3dce287b3c8d90f17a87cac1
TrafiKen <kenzo.salaun@campus-la-chataigneraie.org>
2022-03-22 09:27:21 +0100

    Procédure stocké, affichage médoc

--- Projetcsharp/MedicWindow.xaml.cs
+++ Projetcsharp/MedicWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows;
 
 namespace Projetcsharp
 {
@@ -7,14 +10,36 @@ namespace Projetcsharp
     /// </summary>
     public partial class MedicWindow : Window
     {
-        public MedicWindow()
+        public SqlConnection Conn { get; set; }
+        public MedicWindow(SqlConnection conn)
         {
             InitializeComponent();
+            Conn = conn;
+
         }
 
         private void btnDeconnect_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var proc = "PS_Affichage_Medicament";
+            var command = new SqlCommand(proc, Conn);
+            command.CommandType = CommandType.StoredProcedure;
+            var data = command.ExecuteReader();
+
+            
+            
+            DataTable dt = new DataTable();
+            dt.Load(data);
+            DGmedoc.ItemsSource = dt.DefaultView;
         }
     }
 }
