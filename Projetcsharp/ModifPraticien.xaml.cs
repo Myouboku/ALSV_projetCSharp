bfb202a8f6c0b402915458e5c8f16fef95911edd
Kenzo <94042385+TrafiKen@users.noreply.github.com>
2022-05-10 09:02:03 +0200

    Correction beug edit praticien + medoc

--- Projetcsharp/ModifPraticien.xaml.cs
+++ Projetcsharp/ModifPraticien.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
@@ -49,6 +52,66 @@ namespace Projetcsharp
             {
                 txtBoxCP.Text = txtBoxCP.Text.Substring(0, txtBoxCP.Text.Length - 1);
             }
+        }
+
+        private void btnModifierPraticien_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using SqlCommand command = new("PS_E_Praticien", Conn);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("PRA_ID", praticien.ID);
+                command.Parameters.AddWithValue("PRA_NOM", txtboxNom.Text);
+                command.Parameters.AddWithValue("PRA_PRENOM", txtboxPrenom.Text);
+                command.Parameters.AddWithValue("PRA_CP", Convert.ToInt32(txtBoxCP.Text));
+                command.Parameters.AddWithValue("PRA_ADRESSE", txtboxAdresse.Text);
+                command.Parameters.AddWithValue("PRA_VILLE", txtBoxVille.Text);
+                command.Parameters.AddWithValue("DIS_ID", ((Combobox)cbBoxDiscipline.SelectedItem).Id);
+                var datas = command.ExecuteReader();
+                datas.Read();
+
+                PraticienWindow window = new(Conn);
+                window.ShowDialog();
+                Close();
+
+            }
+            catch (Exception erreur2)
+            {
+                MessageBox.Show("Connexion échouée :\n\n" + erreur2);
+            }
+        }
+
+        private void WindowsLoading(object sender, RoutedEventArgs e)
+        {
+            using SqlCommand comdPratic = new("PS_Recuperation_PraticienId", Conn);
+            comdPratic.CommandType = CommandType.StoredProcedure;
+            comdPratic.Parameters.AddWithValue("PRA_ID", praticien.ID);
+            var datas = comdPratic.ExecuteReader();
+            datas.Read();
+            txtboxNom.Text = datas["PRA_NOM"].ToString();
+            txtboxPrenom.Text = datas["PRA_PRENOM"].ToString();
+            txtboxAdresse.Text = datas["PRA_ADRESSE"].ToString();
+            txtBoxCP.Text = datas["PRA_CP"].ToString();
+            txtBoxVille.Text = datas["PRA_VILLE"].ToString();
+
+            //Discipline
+            List<Combobox> listDis = new List<Combobox>();
+            string ProcDis = "PS_ListeDisipline";
+            var cmdDis = new SqlCommand(ProcDis, Conn);
+            SqlDataReader drDis = cmdDis.ExecuteReader();
+            while (drDis.Read())
+            {
+                listDis.Add(new Combobox
+                {
+                    Id = (Int16)drDis["DIS_ID"],
+                    Nom = (string)drDis["DIS_LIBELLE"]
+                });
+
+            }
+            cbBoxDiscipline.ItemsSource = listDis;
+            cbBoxDiscipline.DisplayMemberPath = "Nom";
+            cbBoxDiscipline.SelectedItem = listDis.First(x => x.Id == (Int16)datas["DIS_ID"]);
+
         }
     }
 }
